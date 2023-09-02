58046fda9b0135f88563c47ddd22b09ae63e8150
Caroline Freitase <freitascaroline.92@gmail.com>
2023-09-02 17:27:23 -0400

    add vercel analityc

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import GlobalStyled from "./GlobalStyled.js";
-import AOS from "aos";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <GlobalStyled />
+    <Analytics />
   </React.StrictMode>
 );
